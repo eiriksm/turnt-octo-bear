7ea704d3e8100e3f56dd7fdebbb5f051ec0bc1a4
Eirik S. Morland <eirik@morland.no>
2014-09-04 17:18:00 +0200

    Adding the gulpfile.

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp');
+var source = require('vinyl-source-stream');
+var browserify = require('browserify');
+var sass = require('gulp-sass');
+var prefix = require('gulp-autoprefixer');
+var rename = require("gulp-rename");
+
+gulp.task('scripts', function() {
+  browserify('./static/app.js')
+    .bundle()
+    .pipe(source('out.js'))
+    .pipe(gulp.dest('./static'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(['static/*', '!static/out.js']  , ['scripts']);
+});
+
+gulp.task('watch2', function() {
+  gulp.watch(['static/scss/**/*'], ['scss']);
+});
+
+gulp.task('scss', function() {
+  return gulp.src('./static/scss/main.scss')
+    .pipe(sass({
+      errLogToConsole: true,
+      error: function(err) {
+      }
+    }))
+    .pipe(prefix("last 1 version", "> 1%", "ie 8", "ie 7"))
+    .pipe(rename('css.min.css'))
+    //.pipe(source('css.min.css'))
+    .pipe(gulp.dest('static/build'));
+});
+
+
+gulp.task('default', ['scripts']);
